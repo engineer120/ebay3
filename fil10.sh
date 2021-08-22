c0443139e00f22dcaeeecfb216aadaa16e7eae62
engineer <nathandimonekene16@gmail.com>
2021-08-22 21:00:44 +0000

    my first language

---
+++ fil10.sh
@@ -0,0 +1,3 @@
+#hello we learning maven installation using server in our aws account 
+echo "life is good in landmark"
+
---
+++ file10.sh
@@ -0,0 +1,2 @@
+#hello we learning script in landmark technology
+echo "life in good is landmark with all engineering "
